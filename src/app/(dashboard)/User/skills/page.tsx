cc38e340cd0c54e3279fd59ac760885486f99760
Nawaj2417 <mdnawajn27@gmail.com>
2025-02-17 01:32:29 +0545

    update

--- src/app/(dashboard)/User/skills/page.tsx
+++ src/app/(dashboard)/User/skills/page.tsx
@@ -5,28 +5,40 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-        <h2 className='text-3xl text-white text-center py-2 font-semibold'>Improve  Your skills</h2>
-        <div className=" bg-[#AF8DFF] p-2 rounded-xl text-white my-4 text-center text-xl">
-      <p>  With our course, we offer tests like typing speed tests and
-        <br />
-         coding logic-building exercises to enhance your skills.</p>
-        </div>
+    <h2 className='text-3xl font-semibold text-center capitalize text-white my-4'>Resume Builder</h2>
 
-<div className="cardshow flex justify-center gap-5 my-6">
-    <Dashboardcard content='typing text' className='bg-green-500 cursor-pointer text-white text-3xl capitalize '  />
-    <Dashboardcard content='coding test' className='bg-red-500 cursor-pointer  text-3xl capitalize text-white '  />
-    <Dashboardcard content='mock test' className='bg-purple-500 cursor-pointer  text-3xl capitalize text-white '  />
+
+<div className="jobshow flex justify-center mx-auto my-4  items-center w-[80%] h-[500px] bg-[#af8dff3c] p-2 rounded-xl text-white  text-center text-xl">
+     <h3 className='text-2xl'>we will update soon...</h3>
+    </div>
+
 </div>
-
-
-        <div className="slider-test w-full m-auto">
-            <h2 className='text-3xl text-white text-center py-2 font-semibold'>Our Courses</h2>
-    <div className="bg-[#af8dff54] px-14 rounded-xl w-full h-[380px] flex items-center my-4 text-center text-xl">
-    <Slidercourse/>
-    </div>
-        </div>
-    </div>
   )
 }
 
 export default page
+
+
+
+//     <div>
+//         <h2 className='text-3xl text-white text-center py-2 font-semibold'>Improve  Your skills</h2>
+//         <div className=" bg-[#AF8DFF] p-2 rounded-xl text-white my-4 text-center text-xl">
+//       <p>  With our course, we offer tests like typing speed tests and
+//         <br />
+//          coding logic-building exercises to enhance your skills.</p>
+//         </div>
+
+// <div className="cardshow flex justify-center gap-5 my-6">
+//     <Dashboardcard content='typing text' className='bg-green-500 cursor-pointer text-white text-3xl capitalize '  />
+//     <Dashboardcard content='coding test' className='bg-red-500 cursor-pointer  text-3xl capitalize text-white '  />
+//     <Dashboardcard content='mock test' className='bg-purple-500 cursor-pointer  text-3xl capitalize text-white '  />
+// </div>
+
+
+//         <div className="slider-test w-full m-auto">
+//             <h2 className='text-3xl text-white text-center py-2 font-semibold'>Our Courses</h2>
+//     <div className="bg-[#af8dff54] px-14 rounded-xl w-full h-[380px] flex items-center my-4 text-center text-xl">
+//     <Slidercourse/>
+//     </div>
+//         </div>
+//     </div>
