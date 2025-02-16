ceae20beee9eafdc258a6922c46b4dee61e5798b
Nawaj2417 <mdnawajn27@gmail.com>
2025-02-16 19:50:19 +0545

    update

---
+++ src/app/(project)/(Home)/jobs/[jobid]/page.tsx
@@ -0,0 +1,31 @@
+"use client"
+import Jobdescription from '@/components/Landingpage/Jobdescription'
+import { useAppDispatch, useAppSelector } from '@/Ruduxtoolkit/hook'
+import { fetchJobById } from '@/Ruduxtoolkit/jobSlice'
+import React, { use, useEffect } from 'react'
+
+const page = ({params}:any) => {
+  const param:any = use(params)
+    
+      const dispatch = useAppDispatch()
+      const {selectedJob} = useAppSelector((state)=>state.job)
+        // console.log(selectedJob)
+      
+        useEffect(()=>{
+          const getparams = async ()=>{
+            const params =  decodeURIComponent(param.jobid).split("_")[1]
+            // console.log(param)
+            
+           await dispatch(fetchJobById(params))
+          }
+          getparams()
+        },[])
+        console.log(selectedJob)
+  return (
+    <div>
+        <Jobdescription data={selectedJob}  />
+    </div>
+  )
+}
+
+export default page
--- src/components/Landingpage/Jobsection.tsx
+++ src/components/Landingpage/Jobsection.tsx
@@ -1,26 +1,48 @@
-"use client"
+"use client";
+
 import React, { useEffect } from "react";
 import Jobcard from "./Jobcard";
 import Link from "next/link";
 import { useAppDispatch, useAppSelector } from "@/Ruduxtoolkit/hook";
 import { fetchJobs } from "@/Ruduxtoolkit/jobSlice";
+import { Skeleton } from "../ui/skeleton";
 
 const Jobsection = () => {
-  const {jobs} = useAppSelector((state) => state.job)
-   const dispatch = useAppDispatch()
-   useEffect(() => {
-     dispatch(fetchJobs())
-   }, [dispatch])
-   console.log(jobs)
+  const { jobs, loading } = useAppSelector((state) => state.job);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchJobs());
+  }, [dispatch]);
 
   return (
-    <section className="bg-[#2E2835] w-full h-auto px-2 py-5  sm:p-10">
+    <section className="bg-[#2E2835] w-full h-auto px-2 py-5 sm:p-10">
       <h1 className="text-xl sm:text-3xl uppercase font-semibold text-center text-white p-10">
-        Recent job
+        Recent Jobs
       </h1>
-      <div className="job-container  flex flex-wrap justify-center gap-5">
-        {jobs.map((data)=>   <Link href={`/jobs/${data.title}_${data.$id}`} key={data.title}><Jobcard  data={data} /></Link>)}
-     
+      <div
+        className={`job-container flex flex-wrap justify-center gap-5 `}
+      >
+        {loading || !jobs.length ? (
+          // Show four skeleton loaders in a centered grid
+          Array.from({ length: 4 }).map((_, index) => (
+            <div key={index} className="flex flex-col space-y-3 w-[300px]">
+              {/* Adjusted Skeleton Colors */}
+              <Skeleton className="h-[200px] w-full rounded-xl bg-gray-700 animate-pulse" />
+              <div className="space-y-2">
+                <Skeleton className="h-4 w-full bg-gray-600 animate-pulse" />
+                <Skeleton className="h-4 w-[80%] bg-gray-600 animate-pulse" />
+              </div>
+            </div>
+          ))
+        ) : (
+          // Render job cards if jobs are available
+          jobs.map((data) => (
+            <Link href={`/jobs/${data.title}_${data.$id}`} key={data.$id}>
+              <Jobcard data={data} />
+            </Link>
+          ))
+        )}
       </div>
     </section>
   );
