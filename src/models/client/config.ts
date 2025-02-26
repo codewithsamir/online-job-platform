c30e3b4cd3fae62efb05aa87c0da97761ff3cc63
Nawaj2417 <mdnawajn27@gmail.com>
2025-02-26 06:59:14 +0545

    update

--- src/models/client/config.ts
+++ src/models/client/config.ts
@@ -1,6 +1,7 @@
 import env from "@/app/env";
 
 import { Client, Account, Avatars, Databases, Storage } from "appwrite";
+import { jobs } from "../name";
 
 const client = new Client()
     .setEndpoint(env.appwrite.endpoint) // Your API Endpoint
@@ -10,6 +11,12 @@ const databases = new Databases(client)
 const account = new Account(client);
 const avatars = new Avatars(client);
 const storage = new Storage(client);
+
+
+//job collection on show real times
+client.subscribe(jobs, (response) => {
+    console.log('New update: on job collection', response);
+});
 
 
 export { client, databases, account, avatars, storage}
